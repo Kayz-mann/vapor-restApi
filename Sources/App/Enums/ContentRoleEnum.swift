9157bfb5be5614cf6c3ffd1d307b16e15d5c1d11
Kayode <kayodefredrickbalogun@gmail.com>
2024-08-19 16:05:07 +0100

    add: Session && Article Model

---
+++ Sources/App/Enums/ContentRoleEnum.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Balogun Kayode on 19/08/2024.
+//
+
+import Foundation
+
+enum ContentRoleEnum: String {
+    case article
+    case podcast
+    case blogpost
+    case tutorial
+    case livestream
+    
+}
